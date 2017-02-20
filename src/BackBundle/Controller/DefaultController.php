8b52e685a55a440e91465ec5188955819af5ae21
Jérémie BARTOLI <jeremie.bartoli@free.fr>
2017-02-20 15:03:24 +0100

    Added basic front dispatch Added error catch

--- src/BackBundle/Controller/DefaultController.php
+++ src/BackBundle/Controller/DefaultController.php
@@ -5,6 +5,9 @@ namespace BackBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/admin/")
+ */
 class DefaultController extends Controller
 {
     /**
