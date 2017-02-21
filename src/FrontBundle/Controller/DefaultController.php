7926774935fda8e8d4e5bb6c601b7443d5f5e080
Jérémie BARTOLI <jeremie.bartoli@free.fr>
2017-02-21 14:14:29 +0100

    Updated FosUser Ressources dependencies

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -4,14 +4,39 @@ namespace FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('FrontBundle:Default:index.html.twig');
+        $defaultData = array();
+        $form = $this->createFormBuilder($defaultData)
+            ->add('title', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('theme', ChoiceType::class, array('choices' => array('Aventure', 'Action', 'Science')))
+            ->add('categories', ChoiceType::class, array('choices' => array('Roman', 'Magazine', 'Nouvelle')))
+            ->add('send', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+
+        }
+
+        return $this->render('FrontBundle:Default:index.html.twig', array('form' => $form->createView()));
     }
+
 }
